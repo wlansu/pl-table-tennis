bba6899e5fb424f2fed1d2c02eff4a6683a44a3f
Wouter Lansu <wfrlansu@gmail.com>
2014-08-19 13:39:10 +0200

    changed settings

--- table_tennis/wsgi.py
+++ table_tennis/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
